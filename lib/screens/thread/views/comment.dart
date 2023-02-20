0d595f54915a5018cefdc13025561fef06f14f5b
Nguyen Trong Tin <trongtin045@gmail.com>
2023-02-20 22:44:41 +0700

    updates

--- lib/screens/thread/views/comment.dart
+++ lib/screens/thread/views/comment.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:voz/screens/webview/webview.dart';
 
 import '../../../interfaces/comment.dart';
@@ -109,10 +110,47 @@ class Comment {
                   itemBuilder: (_, index) {
                     return Html(
                       data: comment.contents[index],
+                      style: {
+                        'blockquote': Style(
+                          maxLines: 3,
+                          border: const Border(
+                            top: BorderSide(
+                              width: 1,
+                              color: Color(0xffd3d5d7),
+                            ),
+                            bottom: BorderSide(
+                              width: 1,
+                              color: Color(0xffd3d5d7),
+                            ),
+                            left: BorderSide(
+                              width: 3,
+                              color: Color(0xffff944d),
+                            ),
+                            right: BorderSide(
+                              width: 1,
+                              color: Color(0xffd3d5d7),
+                            ),
+                          ),
+                        ),
+                        '.bbCodeBlock-title': Style(
+                          padding: const EdgeInsets.all(5),
+                          backgroundColor: const Color(0xffe7e8e9),
+                        ),
+                        '.bbCodeBlock-content': Style(
+                          padding: const EdgeInsets.only(left: 5, right: 5, bottom: 5),
+                          backgroundColor: const Color(0xffe2e3e5),
+                        ),
+                        '.js-expandLink': Style(
+                          display: Display.NONE,
+                        ),
+                      },
                       onLinkTap: (url, context, attributes, element) {
                         Get.to(
                           () => WebviewScreen(url: url!),
                         );
+                      },
+                      onImageTap: (url, context, attributes, element) {
+                        if (url != null) launchUrl(Uri.parse(url));
                       },
                     );
                   },
